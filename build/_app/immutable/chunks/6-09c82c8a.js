be93b19d68be70a5719322781fdd35dfeaa711fb
Dibash Sapkota _ <92871130+SapkotaDibash@users.noreply.github.com>
2023-02-10 08:18:15 +0545

    Add files via upload

--- build/_app/immutable/chunks/6-09c82c8a.js
+++ build/_app/immutable/chunks/6-09c82c8a.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-189548d5.js";import{default as t}from"../components/pages/services/_page.svelte-63692576.js";export{t as component,r as universal};
+import{_ as r}from"./_page-9c76c2d7.js";import{default as t}from"../components/pages/services/_page.svelte-63692576.js";export{t as component,r as universal};
