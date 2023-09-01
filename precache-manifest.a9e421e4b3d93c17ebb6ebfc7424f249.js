ccf76055d365dc32d046c1bf374e4b0ba56806ab
Dibash Sapkota _ <92871130+SapkotaDibash@users.noreply.github.com>
2023-09-01 15:38:59 +0545

    Lekha samiti detail added and minor changes.

---
+++ precache-manifest.a9e421e4b3d93c17ebb6ebfc7424f249.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "291f0af33c00121986e3",
+    "url": "/static/css/main.b8a75d30.chunk.css"
+  },
+  {
+    "revision": "291f0af33c00121986e3",
+    "url": "/static/js/main.291f0af3.chunk.js"
+  },
+  {
+    "revision": "cf042e081f8a87233510",
+    "url": "/static/js/1.cf042e08.chunk.js"
+  },
+  {
+    "revision": "4cc46b6bddecd2641e06",
+    "url": "/static/js/2.4cc46b6b.chunk.js"
+  },
+  {
+    "revision": "69133d183c995e1d60fc",
+    "url": "/static/js/runtime~main.69133d18.js"
+  },
+  {
+    "revision": "f3e0a378c65e08d460ab591049af9040",
+    "url": "/static/media/logo.f3e0a378.png"
+  },
+  {
+    "revision": "d86020078581417548ed13c031a07955",
+    "url": "/index.html"
+  }
+];
